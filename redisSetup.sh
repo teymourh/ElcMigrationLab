5b855bcf0dc5b614c9dd6499625acbd2a5aca225
Teymour Haider <44786279+teymourh@users.noreply.github.com>
2020-05-21 15:06:18 -0700

    Add files via upload

---
+++ redisSetup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Redis install
+sudo yum -y install gcc make # install GCC compiler
+cd /usr/local/src
+sudo wget http://download.redis.io/releases/redis-5.0.6.tar.gz
+sudo tar xvzf redis-5.0.6.tar.gz
+sudo rm -f redis-stable.tar.gz
+cd redis-5.0.6
+sudo make distclean
+sudo make
+sudo cp src/redis-server /usr/local/bin/
+sudo cp src/redis-cli /usr/local/bin/
+redis-server
